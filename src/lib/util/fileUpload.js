d7643b3979c8dbe546a06673ed9b3929e6f2856c
hunchulchoi <nettlica@live.co.kr>
2023-10-06 16:45:23 +0900

    static 경로 테스트

--- src/lib/util/fileUpload.js
+++ src/lib/util/fileUpload.js
@@ -2,23 +2,21 @@ import * as fs from 'fs';
 import { writeFileSync } from 'fs';
 import moment from 'moment';
 
-import {assets, base} from '$app/paths';
-
-import { UPLOAD_PATH } from '$env/static/private';
+import { ASSETS_ROOT, UPLOAD_PATH } from '$env/static/private';
 import { error } from "@sveltejs/kit";
 
 export async function write(file, preservePath = 'jjal') {
-	console.log('base', base, 'assets', assets, 'preservePath', preservePath, 'file', file);
+	console.log('ASSETS_ROOT', ASSETS_ROOT, 'preservePath', preservePath, 'file', file);
 
-	const dir = `/${base}/${UPLOAD_PATH}/${preservePath.replace('..', '').replace('/', '')}/${moment().format(
+	const dir = `${UPLOAD_PATH}/${preservePath.replace('..', '').replace('/', '')}/${moment().format(
 		'YYYY'
 	)}/${moment().format('YYYYMM')}/${moment().format('YYYYMMDD')}`;
 
-	if (!fs.existsSync(`${dir}`)) {
-		fs.mkdirSync(`${dir}`, { recursive: true });
+	if (!fs.existsSync(`${ASSETS_ROOT}${dir}`)) {
+		fs.mkdirSync(`${ASSETS_ROOT}${dir}`, { recursive: true });
 	}
 
-	console.log(dir, fs.existsSync(`${dir}`));
+	console.log(dir, fs.existsSync(`${ASSETS_ROOT}${dir}`));
 
 	const fileName = `${file.name.substring(
 		0,
@@ -27,10 +25,10 @@ export async function write(file, preservePath = 'jjal') {
 		.replace('..', '')
 		.replace('/', '');
 
-	writeFileSync(`${dir}/${fileName}`, Buffer.from(await file.arrayBuffer()));
+	writeFileSync(`${ASSETS_ROOT}${dir}/${fileName}`, Buffer.from(await file.arrayBuffer()));
 
-	console.log(`${dir}/${file.name}`, fs.existsSync(`${dir}/${fileName}`));
+	console.log(`${ASSETS_ROOT}${dir}/${file.name}`, fs.existsSync(`${ASSETS_ROOT}${dir}/${fileName}`));
 
-	if (fs.existsSync(`${dir}/${fileName}`)) return `/${dir}/${fileName}`;
+	if (fs.existsSync(`${ASSETS_ROOT}${dir}/${fileName}`)) return `/${dir}/${fileName}`;
 	else throw error(500, '파일 저장 중에 오류가 발생하였습니다. ㅠㅠ');
 }
