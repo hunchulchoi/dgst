b06248735867159848cf58530afed47e13ba9ac9
hunchulchoi <nettlica@live.co.kr>
2023-10-14 21:57:24 +0900

    읽기 페이지 리플 본문 좌우 패딩

--- src/routes/board/[boardId]/[[pageNo=integer]]/[articleId]/+page.server.js
+++ src/routes/board/[boardId]/[[pageNo=integer]]/[articleId]/+page.server.js
@@ -14,11 +14,13 @@ export const load = async ({ params, locals }) => {
   const session = await locals.getSession();
 
   const filter = { _id: params.articleId, boardId: params.boardId, state: 'write' };
+  
+  const projection = {email:1, nickname:1, title:1, content:1, reads:1, comments:1, likes:1, createdAt:1, read:1, like:1}
 
   const article = await Article.findOneAndUpdate(
     filter,
     { $addToSet: { reads: session?.user?.email } },
-    { new: true, timestamps: false }
+    { new: true, timestamps: false, projection }
   )
     .populate({
       path: 'comments',
@@ -33,7 +35,7 @@ export const load = async ({ params, locals }) => {
     throw error(410, { message: '삭제되었거나 존지하지 않는 게시물입니다.' });
   }
 
-  const author = await User.findOne({ email: article.email }, { photo: 1, introduction: 1 });
+  const author = await User.findOne({ email: article.email }, { photo: 1, introduction: 1 }).lean();
 
   return {
     article: JSON.parse(JSON.stringify(article)),
