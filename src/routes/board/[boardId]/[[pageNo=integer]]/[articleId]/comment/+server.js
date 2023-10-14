365f821fb78c00dbcfe0046d7dec7dbd4019cc9a
hunchulchoi <nettlica@live.co.kr>
2023-10-14 21:51:07 +0900

    리플 삭제할때 게시글 comments에서도 삭제

--- src/routes/board/[boardId]/[[pageNo=integer]]/[articleId]/comment/+server.js
+++ src/routes/board/[boardId]/[[pageNo=integer]]/[articleId]/comment/+server.js
@@ -103,7 +103,6 @@ export async function POST({ request, params, locals }) {
 }
 
 export async function DELETE({ request, params, locals }) {
-  console.debug('request', params);
 
   const boardId = params.boardId;
   const articleId = params.articleId;
@@ -115,16 +114,11 @@ export async function DELETE({ request, params, locals }) {
 
   const session = await locals.getSession();
 
-  console.debug('user', session);
-
   // 권한 검사
   if (!session?.user?.nickname) {
     throw error(401, { message: '권한이 없습니다. 로그인 해주세요' });
   }
-
   const data = await request.json();
-
-  console.log(data);
 
   try {
     const update = await Comment.findOneAndUpdate(
@@ -132,14 +126,15 @@ export async function DELETE({ request, params, locals }) {
       { state: 'deleted', modified_email: session.user.email },
       { timestamps: true }
     );
-
-    console.debug('update', update);
-
+    
     if (!update) {
       throw error(401, {
         message: '삭제 되지 않았습니다. 이미 삭제되었거나 권한이 없는 것 같습니다.'
       });
     }
+    
+   await Article.updateOne({_id:articleId}, {$pull: {comments: data.commentId}})
+   
   } catch (err) {
     console.error(err);
     throw error(err.status, err.body.message ?? '삭제 중에 오류가 발생하였습니다.');
