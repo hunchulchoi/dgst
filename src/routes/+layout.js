f86855bffb01f9c36db1a11cd951bb5d20ae0c22
hunchulchoi <nettlica@live.co.kr>
2023-10-22 19:51:48 +0900

    캐시 유효시간 30초

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,11 @@
-export const load = ({ url, data }) => {
+export const load = ({ url, data, setHeaders }) => {
   const { pathname, search } = url;
 
   console.log('layout alarmCount', data.alarmCount)
+  
+  setHeaders({
+    'cache-control': 'max-age: 30'
+  })
 
   return {
     pathname: `${pathname}${search || ''}`,
