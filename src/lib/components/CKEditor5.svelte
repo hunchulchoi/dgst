83834e011938e2ed53b7095fd460b1fe516a5ef2
hunchulchoi <nettlica@live.co.kr>
2023-10-22 12:59:27 +0900

    유투부 숏

--- src/lib/components/CKEditor5.svelte
+++ src/lib/components/CKEditor5.svelte
@@ -83,14 +83,14 @@
       extraProviders:[
         {
           name: 'youtube shorts',
-          url:/^youtube\.com\/shorts\/([\w-]+)(?:\/\?start=(\d+))?/,
+          url:/^youtube\.com\/shorts\/([\w-]+)(?:\/\?si=([\w-]+))?/,
           html: match=>{
             const id = match[ 1 ];
-            const time = match[ 2 ];
+            const si = match[ 2 ];
 
             return (
-                    '<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
-                    `<iframe src="https://www.youtube.com/embed/${ id }${ time ? `?start=${ time }` : '' }" ` +
+                    '<div style="position: relative;width:100%;padding-bottom:177.777%;">' +
+                    `<iframe src="https://www.youtube.com/embed/${ id }${ si ? `?si=${ si }` : '' }" ` +
                     'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
                     'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
                     '</iframe>' +
