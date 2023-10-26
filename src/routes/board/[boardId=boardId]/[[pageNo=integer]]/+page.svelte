dd7151b9881a80f8d1ae2e608f4ba71d17ba0736
hunchulchoi <nettlica@live.co.kr>
2023-10-26 23:26:18 +0900

    페이징 7개로 제한

--- src/routes/board/[boardId=boardId]/[[pageNo=integer]]/+page.svelte
+++ src/routes/board/[boardId=boardId]/[[pageNo=integer]]/+page.svelte
@@ -76,7 +76,7 @@
             >
             {#each Array((data.end - data.start)) as _, i}
               <PaginationItem
-                active={(!$page.params.pageNo && (i-data.start) === 0) || (i + 1 + data.start) == $page.params.pageNo}
+                active={(!$page.params.pageNo && (data.start -i) === 1) || (i + data.start) == $page.params.pageNo}
               >
                 <PaginationLink href={`/board/${$page.params.boardId}/${i + data.start}`}>
                   {i + data.start}
