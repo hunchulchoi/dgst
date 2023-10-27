7ab336cad0b3c2b5ede4e5fe630746e89b9ea4a7
hunchulchoi <nettlica@live.co.kr>
2023-10-27 18:16:06 +0900

    레이아웃 헤더 캐시 제거

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -2,10 +2,6 @@ export const load = ({ url, data, setHeaders }) => {
   const { pathname, search } = url;
 
   console.log('layout alarmCount', data.alarmCount)
-  
-  setHeaders({
-    'cache-control': 'max-age: 30'
-  })
 
   return {
     pathname: `${pathname}${search || ''}`,
