a3f89434fe6969acfbce43348a82a8e62c156f5a
hunchulchoi <nettlica@live.co.kr>
2023-11-08 21:10:58 +0900

    알람 삭제 안하고 2일치만 보여줌

--- src/routes/board/[boardId=boardId]/[[pageNo=integer]]/[articleId]/comment/+server.js
+++ src/routes/board/[boardId=boardId]/[[pageNo=integer]]/[articleId]/comment/+server.js
@@ -11,7 +11,7 @@ connectDB();
 export async function GET({ params, locals }) {
   const boardId = params.boardId;
   const articleId = params.articleId;
-  
+
 
   if (!boardId || !articleId) {
     console.error('invalid', params);
@@ -25,28 +25,28 @@ export async function GET({ params, locals }) {
       { articleId: params.articleId, boardId: params.boardId },
       { _id: 1, photo: 1, nickname: 1, createdAt: 1, image: 1, email: 1, content: 1, depth:1, parentCommentId: 1, parentCommentNickname: 1 , state:1, likes:1, like:1}
     ).sort('createdAt');
-    
+
   } catch (err) {
     console.error('댓글 목록 실패', err);
     throw error(500, { message: '데이터를 가져오는 중에 오류가 발생하였습니다.ㅜㅜ' });
   }
-  
+
   const commentsTree = JSON.parse(JSON.stringify(convertToTree(comments)));
-  
+
   const session = await locals.getSession();
-  
+
   if (session?.user?.nickname) {
     commentsTree.forEach((c) => {
-      
+
       console.log(session.user.email, c.likes,c.likes.includes(session.user.email))
-      
+
       c.liked = c.likes.includes(session.user.email);
       delete c.likes;
     })
-    
+
     console.log('1 commentsTree', commentsTree)
   }
-  
+
   console.log('session', session)
   console.log('commentsTree', commentsTree)
 
@@ -80,15 +80,15 @@ export async function POST({ request, params, locals }) {
   if (image) {
     storeFileName = await write(image,  session.user.email,'jjal');
   }
-  
+
   const parentCommentId = data.get('parentCommentId');
-  
+
   let parentComment;
-  
+
   if(parentCommentId) {
     parentComment = await Comment.findById(parentCommentId);
   }
-  
+
   //console.log('parentComment', parentComment);
 
   try {
@@ -118,19 +118,21 @@ export async function POST({ request, params, locals }) {
 
     // 내글이 아닐때 알림
     if(article.email !== session.user.email){
-        const alarm = await Alarm.findOneAndUpdate({email: article.email, articleId: articleId}
-          , {$set:{title: article.title, boardId: boardId}, $addToSet: {comments: comment._id}}
-          , {upsert: true, new: true}).lean();
+        if(!parentComment.email || parentComment.email !== article.email){
+            const alarm = await Alarm.findOneAndUpdate({email: article.email, articleId: articleId}
+                , {$set:{title: article.title, boardId: boardId, readAt: null}, $addToSet: {comments: comment._id}}
+                , {upsert: true, new: true}).lean();
+        }
     }
-    
+
     // 내 댓글이 아닐때 알림
     if(parentComment){
       if(parentComment.email !== session.user.email){
         const alarm = await Alarm.findOneAndUpdate({email: parentComment.email, articleId: articleId, comment: parentComment.id}
-          , {$set:{title: article.title, boardId: boardId, comment: parentComment.id, commentContent: parentComment.content}
+          , {$set:{title: article.title, boardId: boardId, comment: parentComment.id, commentContent: parentComment.content, readAt: null}
             , $addToSet: {comments: comment._id}}
           , {upsert: true, new: true}).lean();
-        
+
         //console.log('alarm', alarm);
       }
     }
