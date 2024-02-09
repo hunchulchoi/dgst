d1e8d78a0cd72a436cfc272a5c63a2764fec9a6f
hunchulchoi <nettlica@live.co.kr>
2024-02-09 22:13:48 +0900

    진천의 아들 플랭카드

---
+++ src/lib/shared/stores/ccd.js
@@ -0,0 +1,14 @@
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
+
+const initialValue = browser ? window.localStorage.getItem('ccd') ?? true : true;
+
+const ccd = writable(initialValue);
+
+ccd.subscribe((value) => {
+  if (browser) {
+    window.localStorage.setItem('ccd', value);
+  }
+});
+
+export default ccd;
