747a1b2a5b3b66cd20e9041b0b50b7e9d1bd57cd
hunchulchoi <nettlica@live.co.kr>
2024-02-24 15:04:52 +0900

    대구의 딸 플랭카드 원복

--- src/routes/board/[boardId=boardId]/[[pageNo=integer]]/+page.svelte
+++ src/routes/board/[boardId=boardId]/[[pageNo=integer]]/+page.svelte
@@ -45,7 +45,7 @@
 
 <main class="container my-md-2" style="min-height: 50vh">
 
-  <Offcanvas isOpen={$ccd} header="장하다 - dgst"
+  <!--<Offcanvas isOpen={$ccd} header="장하다 - dgst"
              toggle={()=>ccd.set(false)}
              fade={true}
              class="text-center bg-secondary text-dark rounded-bottom-4"
@@ -64,7 +64,7 @@
  pointer-events: none;">
       <Confetti x={[-5, 5]} y={[0, 0.1]} delay={[500, 2000]} infinite duration=5000 amount=200 fallDistance="100vh" />
     </div>
-  </Offcanvas>
+  </Offcanvas>-->
 
 
 
