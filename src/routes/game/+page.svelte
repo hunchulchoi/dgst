d90ece07f2440fd602fab62a058d850b06fefd4f
hunchulchoi <nettlica@live.co.kr>
2024-03-22 15:18:06 +0900

    game

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,3 +1,5 @@
-<iframe src="https://retropang.daonamu.com/embed/24151" width="100%" height="100%" frameborder="no"
+<iframe src="https://retropang.daonamu.com/embed/24151" width="100%" height="700"
+        max-width="600"
+        frameborder="no"
         allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"
 ></iframe>
