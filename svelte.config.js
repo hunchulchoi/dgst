d1f341e654c66b6ae019f3ce490e4f6134ab4821
hunchulchoi <nettlica@live.co.kr>
2024-12-08 20:26:12 +0900

    assets  설정 변경

--- svelte.config.js
+++ svelte.config.js
@@ -7,9 +7,8 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    appDir: 'static',
     paths:{
-      assets: 'static/',
+      assets: 'static',
       relative: true,
     },
     prerender: {
