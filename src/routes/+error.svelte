dbcabb92845f0c51e0581801207380d799e6cd82
hunchulchoi <nettlica@live.co.kr>
2025-10-19 22:00:14 +0900

    fix: +error.svelte에서 page store 복원
    - error page는 props 대신 page store 사용 필요
    - error?.message 옵셔널 체이닝 추가
    - SvelteKit error page 표준 따름
    
    에러 페이지 수정 🐛

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,24 +2,24 @@
   import { Button, Card, CardBody, CardFooter, CardHeader, CardText, Icon, Image } from '@sveltestrap/sveltestrap';
 
   import { goto } from '$app/navigation';
+  import { page } from '$app/stores';
   
-  // Svelte 5 Runes
-  let { status, error } = $props();
+  // Svelte 5 Runes - page store 필요 (error page는 예외)
 </script>
 
 <Card class="m-5 shadow rounded-4" style="max-width: 500px">
-    <CardHeader class="text-bg-warning bg-opacity-25"><h3>Ooooops!-{status}</h3></CardHeader>
+    <CardHeader class="text-bg-warning bg-opacity-25"><h3>Ooooops!-{$page.status}</h3></CardHeader>
     <Image src="/logo/twitter_header_photo_2.png" alt="데게실버타운 로고"/>
     <CardBody>
         <CardText class="p-2">
-          {#if status===500}에러가 발생하였습니다.{/if}
-            <div class="fs-3">{status}</div>
-            <div class="fs-4">{error.message}</div>
-          {#if status===500}소스 작업 하는 중일 수 있습니다. 새로고침을 해보세요{/if}
+          {#if $page.status===500}에러가 발생하였습니다.{/if}
+            <div class="fs-3">{$page.status}</div>
+            <div class="fs-4">{$page.error?.message || '알 수 없는 오류'}</div>
+          {#if $page.status===500}소스 작업 하는 중일 수 있습니다. 새로고침을 해보세요{/if}
         </CardText>
     </CardBody>
     <CardFooter class="d-flex justify-content-end p-3">
-      {#if status===500}
+      {#if $page.status===500}
       <Button onclick={()=>location.reload()} class="me-2"><Icon name="arrow-repeat" class="pe-1"/> 새로고침</Button>
       {/if}
       <Button onclick={()=>goto('/')} class="me-2"><Icon name="house" class="pe-1"/> Home</Button>
