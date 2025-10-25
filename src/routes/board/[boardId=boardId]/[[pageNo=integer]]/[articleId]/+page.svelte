9fb6606c06aa966804acf6ba2bbfc9b46da90d7b
hunchulchoi <nettlica@live.co.kr>
2025-10-26 00:49:54 +0900

    OG 메타 태그 description을 닉네임과 내용 20글자로 변경

--- src/routes/board/[boardId=boardId]/[[pageNo=integer]]/[articleId]/+page.svelte
+++ src/routes/board/[boardId=boardId]/[[pageNo=integer]]/[articleId]/+page.svelte
@@ -1,13 +1,13 @@
 <svelte:head>
   <!-- Open Graph 메타 태그 -->
   <title>{data.article.title} - 데게실버타운</title>
-  <meta name="description" content={data.article.content.replace(/<[^>]*>/g, '').substring(0, 160)} />
+  <meta name="description" content={`${data.article.nickname} - ${data.article.content.replace(/<[^>]*>/g, '').substring(0, 20)}`} />
   
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="article" />
   <meta property="og:url" content="https://www.dgst.me{data.article._id}" />
   <meta property="og:title" content={data.article.title} />
-  <meta property="og:description" content={data.article.content.replace(/<[^>]*>/g, '').substring(0, 160)} />
+  <meta property="og:description" content={`${data.article.nickname} - ${data.article.content.replace(/<[^>]*>/g, '').substring(0, 20)}`} />
   <meta property="og:image" content="https://www.dgst.me/logo/twitter_header_photo_2.png" />
   <meta property="og:site_name" content="데게실버타운" />
   
@@ -15,7 +15,7 @@
   <meta property="twitter:card" content="summary_large_image" />
   <meta property="twitter:url" content="https://www.dgst.me{data.article._id}" />
   <meta property="twitter:title" content={data.article.title} />
-  <meta property="twitter:description" content={data.article.content.replace(/<[^>]*>/g, '').substring(0, 160)} />
+  <meta property="twitter:description" content={`${data.article.nickname} - ${data.article.content.replace(/<[^>]*>/g, '').substring(0, 20)}`} />
   <meta property="twitter:image" content="https://www.dgst.me/logo/twitter_header_photo_2.png" />
   
   <script async src="//www.tiktok.com/embed.js"></script>
