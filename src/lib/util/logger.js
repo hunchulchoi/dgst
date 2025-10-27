6c8df71a4fb63e2b624fb8f8cf37f72100ada1e0
hunchulchoi <nettlica@live.co.kr>
2025-10-27 20:01:21 +0900

    sharp rotate 추가

--- src/lib/util/logger.js
+++ src/lib/util/logger.js
@@ -4,14 +4,6 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const logger = pino({
   level: isDevelopment ? 'debug' : 'info',
-  ...(isDevelopment && {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-      },
-    },
-  }),
 });
 
 export default logger;
