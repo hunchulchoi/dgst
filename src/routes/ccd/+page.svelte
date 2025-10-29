5b444dd3b8090d2b04e47a237346ecc87b382e33
hunchulchoi <nettlica@live.co.kr>
2025-10-29 21:00:32 +0900

    fix: /ccd 페이지에서 서버 사이드 goto 오류 해결
    - browser 체크를 추가하여 클라이언트에서만 goto 실행

---
+++ src/routes/ccd/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { browser } from "$app/environment";
+    
+    if (browser) {
+        goto('/auth/signin');
+    }
+</script>
--- src/routes/login/+page.svelte
+++
@@ -1,5 +0,0 @@
-// auth/signin 페이지로 리다이렉트
-import { goto } from '$app/navigation';
-
-goto('/auth/signin');
-
