e14b7b567029758d8d288987cce283c85bd5f380
hunchulchoi <nettlica@live.co.kr>
2026-01-06 17:48:54 +0900

    시골을신

--- src/lib/components/banner.svelte
+++ src/lib/components/banner.svelte
@@ -17,7 +17,7 @@ const bannerContent =
            toggle={()=>ccd.set(false)}
            fade={true}
            class="text-center text-dark rounded-bottom-4"
-           style="height:100%;background: linear-gradient(90deg, rgba(211,209,247,1) 0%, rgba(150,150,146,1) 70%,
+           style="background: linear-gradient(90deg, rgba(211,209,247,1) 0%, rgba(150,150,146,1) 70%,
            rgba(111,112,101,1) 100%);"
            placement="top">
   <div class="neon">{@html bannerContent}</div>
